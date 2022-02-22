572979116f3d7f68ef5b16684b00d2347e3d2092
Jose Cabrera <jose@codium.team>
2022-02-22 15:19:23 +0100

    Refactor isAdvantage & isWinScore

--- src/getScore.js
+++ src/getScore.js
@@ -3,10 +3,12 @@ function getScore(m_score1, m_score2) {
 
   if (hasSameScore(m_score1, m_score2)) {
     score = calculateSameScore(m_score1);
-  } else if (isAdvantageOrWin(m_score1, m_score2)) {
-    score = calculateAdvantageOrWinScore(m_score1, m_score2, score);
+  } else if (isAdvantage(m_score1, m_score2)) {
+    score = calculateAdvantageScore(m_score1, m_score2);
+  } else if (isWinScore(m_score1, m_score2)) {
+    score = calculateWinScore(m_score1, m_score2, score);
   } else {
-    score = calculateRegularScore(m_score1, score, m_score2);
+    score = calculateRegularScore(m_score1, m_score2);
   }
 
   return score;
@@ -22,49 +24,43 @@ function calculateSameScore(m_score1) {
   return scores[m_score1] || 'Deuce';
 }
 
-function isAdvantageOrWin(m_score1, m_score2) {
-  return m_score1 >= 4 || m_score2 >= 4;
+function isAdvantage(m_score1, m_score2) {
+  return (
+    (m_score1 >= 4 || m_score2 >= 4) && Math.abs(m_score1 - m_score2) === 1
+  );
 }
 
-function calculateRegularScore(m_score1, score, m_score2) {
-  var tempScore = 0;
-  for (var i = 1; i < 3; i++) {
-    if (i === 1) {
-      tempScore = m_score1;
-    } else {
-      score += '-';
-      tempScore = m_score2;
-    }
-    switch (tempScore) {
-      case 0:
-        score += 'Love';
-        break;
-      case 1:
-        score += 'Fifteen';
-        break;
-      case 2:
-        score += 'Thirty';
-        break;
-      case 3:
-        score += 'Forty';
-        break;
-    }
-  }
-  return score;
-}
-
-function calculateAdvantageOrWinScore(m_score1, m_score2, score) {
+function calculateAdvantageScore(m_score1, m_score2) {
+  var score = '';
   var minusResult = m_score1 - m_score2;
+
   if (minusResult === 1) {
     score = 'Advantage player1';
   } else if (minusResult === -1) {
     score = 'Advantage player2';
-  } else if (minusResult >= 2) {
+  }
+
+  return score;
+}
+
+function isWinScore(m_score1, m_score2) {
+  return (m_score1 >= 4 || m_score2 >= 4) && Math.abs(m_score1 - m_score2) > 1;
+}
+
+function calculateWinScore(m_score1, m_score2) {
+  var score = '';
+  if (m_score1 - m_score2 >= 2) {
     score = 'Win for player1';
   } else {
     score = 'Win for player2';
   }
   return score;
+}
+
+function calculateRegularScore(m_score1, m_score2) {
+  var scores = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+
+  return scores[m_score1] + '-' + scores[m_score2];
 }
 
 // THIS IS A HACK FOR TESTING PURPOSES ONLY!!!
