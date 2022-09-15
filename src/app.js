61fd7b05b30377be03173054c92c972f0cafcd10
Jose Cabrera <jose@codium.team>
2022-09-15 17:50:06 +0200

    Reorder function

--- src/app.js
+++ src/app.js
@@ -28,61 +28,61 @@ $(function () {
       addPointPlayerTwo.prop('disabled', true);
     }
   });
+});
 
-  function getScore(m_score1, m_score2) {
-    var score = '';
-    var tempScore = 0;
-    if (m_score1 === m_score2) {
-      switch (m_score1) {
+function getScore(m_score1, m_score2) {
+  var score = '';
+  var tempScore = 0;
+  if (m_score1 === m_score2) {
+    switch (m_score1) {
+      case 0:
+        $('#score').text('Love-All');
+        break;
+      case 1:
+        $('#score').text('Fifteen-All');
+        break;
+      case 2:
+        $('#score').text('Thirty-All');
+        break;
+      default:
+        $('#score').text('Deuce');
+        break;
+    }
+  } else if (m_score1 >= 4 || m_score2 >= 4) {
+    var minusResult = m_score1 - m_score2;
+    if (minusResult === 1) {
+      $('#score').text('Advantage player1');
+    } else if (minusResult === -1) {
+      $('#score').text('Advantage player2');
+    } else if (minusResult >= 2) {
+      $('#score').text('Win for player1');
+    } else {
+      $('#score').text('Win for player2');
+    }
+  } else {
+    for (var i = 1; i < 3; i++) {
+      if (i === 1) {
+        tempScore = m_score1;
+      } else {
+        score += '-';
+        tempScore = m_score2;
+      }
+      switch (tempScore) {
         case 0:
-          $('#score').text('Love-All');
+          score += 'Love';
           break;
         case 1:
-          $('#score').text('Fifteen-All');
+          score += 'Fifteen';
           break;
         case 2:
-          $('#score').text('Thirty-All');
+          score += 'Thirty';
           break;
-        default:
-          $('#score').text('Deuce');
+        case 3:
+          score += 'Forty';
           break;
       }
-    } else if (m_score1 >= 4 || m_score2 >= 4) {
-      var minusResult = m_score1 - m_score2;
-      if (minusResult === 1) {
-        $('#score').text('Advantage player1');
-      } else if (minusResult === -1) {
-        $('#score').text('Advantage player2');
-      } else if (minusResult >= 2) {
-        $('#score').text('Win for player1');
-      } else {
-        $('#score').text('Win for player2');
-      }
-    } else {
-      for (var i = 1; i < 3; i++) {
-        if (i === 1) {
-          tempScore = m_score1;
-        } else {
-          score += '-';
-          tempScore = m_score2;
-        }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
-        }
-      }
-
-      $('#score').text(score);
     }
+
+    $('#score').text(score);
   }
-});
+}
