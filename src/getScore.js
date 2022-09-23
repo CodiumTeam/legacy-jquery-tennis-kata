fb20c3a03b1530436785263e8f2a04fbd44420e7
Jose Cabrera <jose@codium.team>
2022-09-23 09:18:19 +0200

    Refactor production code

--- src/getScore.js
+++ src/getScore.js
@@ -1,56 +1,47 @@
 export function getScore(m_score1, m_score2) {
   var score = '';
-  var tempScore = 0;
-  if (m_score1 === m_score2) {
-    switch (m_score1) {
-      case 0:
-        score = 'Love-All';
-        break;
-      case 1:
-        score = 'Fifteen-All';
-        break;
-      case 2:
-        score = 'Thirty-All';
-        break;
-      default:
-        score = 'Deuce';
-        break;
-    }
-  } else if (m_score1 >= 4 || m_score2 >= 4) {
-    var minusResult = m_score1 - m_score2;
-    if (minusResult === 1) {
-      score = 'Advantage player1';
-    } else if (minusResult === -1) {
-      score = 'Advantage player2';
-    } else if (minusResult >= 2) {
-      score = 'Win for player1';
-    } else {
-      score = 'Win for player2';
-    }
+  if (hasSameScore(m_score1, m_score2)) {
+    score = calculateSameScore(m_score1);
+  } else if (isAdvantageOrWin(m_score1, m_score2)) {
+    score = calculateAdvantageOrWin(m_score1, m_score2);
   } else {
-    for (var i = 1; i < 3; i++) {
-      if (i === 1) {
-        tempScore = m_score1;
-      } else {
-        score += '-';
-        tempScore = m_score2;
-      }
-      switch (tempScore) {
-        case 0:
-          score += 'Love';
-          break;
-        case 1:
-          score += 'Fifteen';
-          break;
-        case 2:
-          score += 'Thirty';
-          break;
-        case 3:
-          score += 'Forty';
-          break;
-      }
-    }
+    score = calculateRegularScore(m_score1, m_score2);
   }
 
   return score;
+}
+
+function calculateRegularScore(m_score1, m_score2) {
+  var scores = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+
+  return scores[m_score1] + '-' + scores[m_score2];
+}
+
+function calculateAdvantageOrWin(m_score1, m_score2, score) {
+  var score = '';
+  var minusResult = m_score1 - m_score2;
+  if (minusResult === 1) {
+    score = 'Advantage player1';
+  } else if (minusResult === -1) {
+    score = 'Advantage player2';
+  } else if (minusResult >= 2) {
+    score = 'Win for player1';
+  } else {
+    score = 'Win for player2';
+  }
+  return score;
+}
+
+function isAdvantageOrWin(m_score1, m_score2) {
+  return m_score1 >= 4 || m_score2 >= 4;
+}
+
+function calculateSameScore(m_score1) {
+  var scores = ['Love-All', 'Fifteen-All', 'Thirty-All'];
+
+  return scores[m_score1] || 'Deuce';
+}
+
+function hasSameScore(m_score1, m_score2) {
+  return m_score1 === m_score2;
 }
