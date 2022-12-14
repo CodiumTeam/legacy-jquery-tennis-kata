78e10b1d3d7bc0af792c18ab17a223a9085da3f6
Jose Cabrera <jose@codium.team>
2022-12-14 20:10:16 +0100

    Add image snaptshop plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      initPlugin(on, config);
+
+      return config;
     },
   },
 });
